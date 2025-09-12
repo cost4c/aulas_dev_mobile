35c4876774aa883b894c8c91c19ef5ec9c4b4b56
Fred Alef Costa Fabiano <20242INN007@cefetes.br>
2025-09-11 21:45:39 -0300

    11/09

--- flamengo/src/main.jsx
+++ flamengo/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Perfil from './components/Perfil.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Perfil />
   </StrictMode>,
 )
